0ef50d18ae0feb643c2da92ab2c83bf553d0f8a1
louwjlab <louwjlabuschagne@gmail.com>
2022-03-29 18:02:07 +0200

    getReservedState method added to marketplace

--- migrations/2_marketplace.js
+++ migrations/2_marketplace.js
@@ -1,5 +1,7 @@
 const Marketplace = artifacts.require('Marketplace');
 const MarketplaceProxy = artifacts.require('MarketplaceProxy');
+const RESERVER_ADDRESS = '0xc073Cf3940C116562FBa21d690FF2121A75844FD';
+const RESERVER_ROLE = web3.utils.soliditySha3('RESERVER_ROLE')
 
 const erc20Addresses = {
   mumbai: {
@@ -8,6 +10,9 @@ const erc20Addresses = {
   development: {
     USDT: '0x3813e82e6f7098b9583fc0f33a962d02018b6803',
   },
+  'polygon_trezor':{
+    USDC: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174',
+  }
 };
 
 const sleep = (ms) => {
@@ -16,6 +21,9 @@ const sleep = (ms) => {
 };
 
 module.exports = async (deployer, network, accounts) => {
+
+  console.log(`deploying Marketplace on ${network} using accounts: ${accounts}`);
+
   if (network !== 'development') {
     await sleep(7500);
   }
@@ -58,5 +66,6 @@ module.exports = async (deployer, network, accounts) => {
   if (network !== 'development') {
     await sleep(7500);
   }
-  await marketplace.grantRole(web3.utils.soliditySha3('RESERVER_ROLE'), '0xc073Cf3940C116562FBa21d690FF2121A75844FD')
+  console.log("Adding Fanie as a reserver");
+  await marketplace.grantRole(RESERVER_ROLE, RESERVER_ADDRESS)
 };
